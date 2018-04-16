2275947adcba4ee7c7bf2b2567de02a7cca3f6c4
Geewon Lee <gwlee1991@gmail.com>
2018-04-16 15:00:35 -0700

    some styling and fix bugs

---
+++ tictactoe/main.js
@@ -0,0 +1,8 @@
+
+// let boxes = document.querySelectorAll('div.box');
+// for (let i = 0; i < boxes.length; i++) {
+//   boxes[i].addEventListener("click", e => {
+//     e.preventDefault();
+//     console.log('hi');
+//   })
+// }
